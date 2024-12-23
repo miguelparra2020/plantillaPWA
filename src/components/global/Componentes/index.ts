80d7b876114360b9a2c099788e2a6245cf2ba44e
@mpaez1 <mpaez@ikatechsolutions.com>
2024-12-22 20:09:30 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -1,3 +1,4 @@
 export { default as Componentes } from "./Componentes.astro"
 export { default as ButtonRouter } from "./ButtonRouter.astro"
 export { default as ButtonGeneral } from "./ButtonGeneral.jsx"
+export { default as CustomImagen } from "./CustomImagen.astro"
