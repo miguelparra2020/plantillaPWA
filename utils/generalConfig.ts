f48fc2f81af2ae933fabdce7d2e047f1644ee4cc
@mpaez1 <mpaez@ikatechsolutions.com>
2024-12-24 19:48:36 -0500

    add-changes

--- utils/generalConfig.ts
+++ utils/generalConfig.ts
@@ -1,8 +1,14 @@
+import ImagenCompany from '@img/user.png';
 export const generalConfig = {
 	name: "Mi compañía",
     NavBar: {
-        nameCompany : "",
-        linksPages: {}
+        nameCompany : "Mi compañía",
+        className: "text-gray-800  hover:text-green-600",
+        logoCompany: ImagenCompany,
+        classLogo: "",
+        classLinks: "text-gray-800  hover:text-green-600",
+        linksPages: {},
+        classUserCar: "bg-white hover:bg-green-600 rounded-full hover:rounded-full",
     }
 	
 }
