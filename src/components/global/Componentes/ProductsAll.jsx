b96a6feba617abb8dde9d2a1ee197355ab1530aa
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-21 10:39:50 -0500

    add-changes

--- src/components/global/Componentes/ProductsAll.jsx
+++ src/components/global/Componentes/ProductsAll.jsx
@@ -16,7 +16,117 @@ const ProductsAll = () => {
 
     return (
         <div className="d-flex flex-col w-full items-center justify-center">
-<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 max-w-screen-lg mx-auto items-center justify-center justify-items-center">
+
+<form
+  className="max-w-lg mx-auto fixed inset-0 flex items-start justify-center top-20 z-50 py-4"
+>
+  <div className="flex">
+    <label
+      htmlFor="search-dropdown"
+      className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+    >
+      Buscar
+    </label>
+    <button
+      id="dropdown-button"
+      data-dropdown-toggle="dropdown"
+      className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+      type="button"
+    >
+      All categories
+      <svg
+        className="w-2.5 h-2.5 ms-2.5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 10 6"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="m1 1 4 4 4-4"
+        />
+      </svg>
+    </button>
+    <div
+      id="dropdown"
+      className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+    >
+      <ul
+        className="py-2 text-sm text-gray-700 dark:text-gray-200"
+        aria-labelledby="dropdown-button"
+      >
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Mockups
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Templates
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Design
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Logos
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div className="relative w-full">
+      <input
+        type="search"
+        id="search-dropdown"
+        className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+        placeholder="Buscar producto..."
+        required
+      />
+      <button
+        type="submit"
+        className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          className="w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+        <span className="sr-only">Buscar</span>
+      </button>
+    </div>
+  </div>
+</form>
+
+
+<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 max-w-screen-lg mx-auto items-center justify-center justify-items-center mt-14">
             {products && products.map((product, index) => (
                 <div key={index} className="w-[92%] mt-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="/">
@@ -25,7 +135,7 @@ const ProductsAll = () => {
                     <div className="px-5 pb-5">
                         <a href="#">
                             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                                {product.name.length > 20 ? product.name.slice(0, 20) + "..." : product.name}</h5>
+                                {product.name.length > 20 ? product.name.slice(0, 25) + "..." : product.name}</h5>
                         </a>
                         <div class="flex items-center mt-2.5 mb-5">
                     <div class="flex items-center space-x-1 rtl:space-x-reverse">
