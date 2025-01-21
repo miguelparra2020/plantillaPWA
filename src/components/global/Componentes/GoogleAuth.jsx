bf459fd911b899aee99019f5c6bc4aed27f6b942
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-20 21:32:50 -0500

    add-changes

--- src/components/global/Componentes/GoogleAuth.jsx
+++ src/components/global/Componentes/GoogleAuth.jsx
@@ -46,38 +46,74 @@ const GoogleAuth = () => {
   return (
     <GoogleOAuthProvider clientId={generalConfig.GoogleAuth.clientId}>
 
-
-<section class="bg-gray-50 dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
-        <div class="flex flex-col justify-center">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none
-             text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Bienvenido a nuestro aplicativo web</h1>
-            <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-              Iniciar sesión es necesario para acceder a los contenidos relacionados con el usuario.
-            </p>
-        </div>
-        <div>
-            <div class="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-                <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
-                    Iniciar sesión con tu cuenta de Google
-                </h2>
-                <form class="mt-8 space-y-6" action="#">
-                {!user && (
-          <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-        )}
-                </form>
-            </div>
-        </div>
-    </div>
+{!user ? (
+  <section className="bg-gray-50 ">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
+      <div className="flex flex-col justify-center">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none
+           text-gray-900 md:text-5xl lg:text-6xl ">Bienvenido a nuestro aplicativo web</h1>
+          <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl ">
+            Iniciar sesión es necesario para acceder a los contenidos relacionados con el usuario.
+          </p>
+      </div>
+      <div>
+          <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl ">
+              <h2 className="text-2xl font-bold text-gray-900 ">
+                  Iniciar sesión con tu cuenta de Google
+              </h2>
+              <form className="mt-8 space-y-6" action="#">
+              {!user && (
+        <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+      )}
+              </form>
+          </div>
+      </div>
+  </div>
 </section>
+):(<div>
 
 
-      {/* <div>
-            <h2>Bienvenido, {user.name}!</h2>
-            <p>Email: {user.email}</p>
+<section className="bg-white  bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] ">
+<div className="py-8 px-4 mx-auto max-w-screen-xl flex flex-col items-center justify-center 
+text-center lg:py-16 z-10 relative">
+  <img className="w-10 h-10 rounded-full" src={user.picture} alt="Rounded avatar" />
+  <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
+    {user.name}
+  </h1>
+  <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48">
+    {user.email}
+  </p>
+  <button
+    onClick={handleLogout}
+    type="button"
+    className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-red-700 bg-red-100 rounded-full hover:bg-red-200"
+  >
+    <span className="text-xs bg-red-600 rounded-full text-white px-4 py-1.5 me-3">Cerrar sesión</span>
+    <svg
+      className="w-2.5 h-2.5 ms-2 rtl:rotate-180"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 6 10"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m1 9 4-4-4-4"
+      />
+    </svg>
+  </button>
+</div>
 
-            <button onClick={handleLogout}>Cerrar sesión</button>
-          </div> */}
+    <div className="bg-gradient-to-b from-blue-50 to-transparent  w-full h-full absolute top-0 left-0 z-0"></div>
+</section>
+</div> ) }
+
+
+
+      
     </GoogleOAuthProvider>
   );
 };
