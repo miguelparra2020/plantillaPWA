9ff171e650d0bd342096456c11a41dfd2efe2f60
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-28 22:53:18 -0500

    add-changes

--- src/components/global/Componentes/MenuView.jsx
+++ src/components/global/Componentes/MenuView.jsx
@@ -4,14 +4,18 @@ import { generalConfig } from "@util/generalConfig";
 
 const MenuView = () => {
   const [openMenu, setOpenMenu] = useState(false);
-
+  const [openChat, setOpenChat] = useState(false);
   const toggleMenu = () => {
-    setOpenMenu(!openMenu); // Cambia el estado
-  };
+    setOpenMenu(!openMenu)
+  }
+  const toggleChat = () => {
+    setOpenChat(!openChat)
+  }
 
   return (
     <div>
-      <div className="fixed bottom-16 right-4 z-50" >
+      {!openChat && (
+        <div className="fixed bottom-20 right-4 z-50" >
       <button onClick={toggleMenu} type="button" className="inline-flex items-center justify-center w-12 h-12 
       font-medium bg-gray-600 rounded-full hover:bg-gray-700 group ring-4 focus:ring-4 ring-gray-200  focus:ring-gray-500
       focus:outline-none ">
@@ -20,7 +24,34 @@ const MenuView = () => {
         </svg>
         <span className="sr-only">Menú</span>
       </button>
-    </div>
+      </div>
+      )}
+      
+    {!openMenu && (
+    <div>    
+      <div className="fixed bottom-20 left-4 z-50">
+        <button
+          onClick={toggleChat}
+          type="button"
+          className="inline-flex items-center justify-center w-12 h-12 
+          font-medium bg-gray-600 rounded-full hover:bg-gray-700 group ring-4 focus:ring-4 ring-gray-200 
+          focus:ring-gray-500 focus:outline-none"
+        >
+          <svg
+            className="w-6 h-6 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+          </svg>
+          <span className="sr-only">Chat</span>
+        </button>
+      </div>
+    </div>)}
+
 
 {openMenu && (
     <div id="menu-content" >
@@ -62,7 +93,38 @@ const MenuView = () => {
     </div>
 </div>  
 )}
-       
+    {openChat && (
+    <div id="menu-content" >
+    <div id="drawer-navigation" className="h-[80vh] p-4  overflow-y-auto transition-transform z-2000  bg-gray-100 
+    w-[100%] " aria-labelledby="drawer-navigation-label">
+        <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase ">Chat</h5>
+        <button onClick={toggleChat} type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" 
+        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 
+        absolute top-2.5 end-2.5 inline-flex items-center justify-center">
+        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+        <span className="sr-only">Close menu</span>
+        </button>
+        <iframe src="https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2025/01/22/23/20250122234411-45T0QKRT.json" title="Botpress Webchat"
+          className="width: 100%; height: 500px;"
+          style={{width: "100%", height: "400px"}}
+          fillRule
+          ></iframe>
+        <div className="py-4 overflow-y-auto">
+        
+        <ul className="space-y-2 font-medium flex items-center justify-end">
+                    <li>
+                    <a href="/contacto" className="bg-slate-400 p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                        <span className="ms-3">Contactar con la empresa </span>
+                    </a>
+                    </li>
+        
+        </ul>
+    </div>
+    </div>
+</div>  
+)}   
 
     </div>
   );
