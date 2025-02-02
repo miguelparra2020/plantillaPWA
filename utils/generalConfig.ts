7fd8450fac5ee2bbc32915938f7af355b995eb95
@mpaez1 <mpaez@ikatechsolutions.com>
2025-02-01 20:47:34 -0500

    add-changes

--- utils/generalConfig.ts
+++ utils/generalConfig.ts
@@ -1,50 +1,131 @@
 import ImagenCompany from '@img/user.png';
+type Idioma = "/en/" | "/es/" | "/pt/" | "/fr/";
+const idioma: Idioma = "/es/"; //Idomas disponibles "/en/" (Inglés) , "/es/" (Español), "/pt/" (Português), "/fr/" (Frances)
 export const generalConfig = {
-	name: "Mi compañía",
+  idioma: idioma,
+  name: "Dinastía Digital",
   NavBar: {
-        nameCompany : "Mi compañía",
-        className: "text-gray-800  hover:text-green-600",
+        nameCompany : "Dinastía Digital",// Máximo 18 carácteres
+        className: "",// Para los estilos de los links hay que entrar al archivo header/Header.astro
         logoCompany: ImagenCompany,
-        classLogo: "",
-        classLinks: "text-gray-800  hover:text-green-600",
         linksPages: [
-          { href: "/", id: "inicio", text: "Inicio", icon: "bi-house-door-fill", active: true,
+          { href: "/", id: "inicio", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Home"
+              case "/es/": return "Inicio"
+              case "/pt/": return "Inicio"
+              case "/fr/": return "Accueil"
+              default: return "Inicio"
+            }
+          })(), icon: "bi-house-door-fill", active: true,
             path1:"M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5",
             path2:""
           },
-          { href: "/productos", id: "productos", text: "Productos", icon: "bi-box-seam", active: true,
+          { href: "/productos", id: "productos", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Products"
+              case "/es/": return "Productos"
+              case "/pt/": return "Produtos"
+              case "/fr/": return "Produits"
+              default: return "Productos"
+            }
+          })(), icon: "bi-box-seam", active: true,
             path1:"M15.528 2.973a.75.75 0 0 1 .472.696v8.662a.75.75 0 0 1-.472.696l-7.25 2.9a.75.75 0 0 1-.557 0l-7.25-2.9A.75.75 0 0 1 0 12.331V3.669a.75.75 0 0 1 .471-.696L7.443.184l.01-.003.268-.108a.75.75 0 0 1 .558 0l.269.108.01.003zM10.404 2 4.25 4.461 1.846 3.5 1 3.839v.4l6.5 2.6v7.922l.5.2.5-.2V6.84l6.5-2.6v-.4l-.846-.339L8 5.961 5.596 5l6.154-2.461z",
             path2:""
           },
-          { href: "/servicios", id: "servicios", text: "Servicios", icon: "bi-tools", active: false,
+          { href: "/servicios", id: "servicios", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Services"
+              case "/es/": return "Servicios"
+              case "/pt/": return "Produtos"
+              case "/fr/": return "Serviços"
+              default: return "Servicios"
+            }
+          })(), icon: "bi-tools", active: true,
             path1:"M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z",
             path2:""
           },
-          { href: "/fotos", id: "fotos", text: "Fotos", icon: "bi-tools", active: true,
+          { href: "/fotos", id: "fotos", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Photos"
+              case "/es/": return "Fotos"
+              case "/pt/": return "Fotos"
+              case "/fr/": return "Photos"
+              default: return "Fotos"
+            }
+          })(), icon: "bi-tools", active: false,
             path1:"M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3",
             path2:"M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z"
           },
-          { href: "/publicaciones", id: "publicaciones", text: "Publicaciones", icon: "bi-journal-text", active: false,
+          { href: "/publicaciones", id: "publicaciones", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Publications"
+              case "/es/": return "Publicaciones"
+              case "/pt/": return "Publicações"
+              case "/fr/": return "Publications"
+              default: return "Publicaciones"
+            }
+          })(), icon: "bi-journal-text", active: false,
             path1:"M4 3.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z",
             path2:"M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1"
           },
-          { href: "/eventos", id: "eventos", text: "Eventos", icon: "bi-calendar-event", active: false,
+          { href: "/eventos", id: "eventos", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Events"
+              case "/es/": return "Eventos"
+              case "/pt/": return "Eventos"
+              case "/fr/": return "Événements"
+              default: return "Eventos"
+            }
+          })(), icon: "bi-calendar-event", active: false,
             path1:"M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708",
             path2:""
           },
-          { href: "/citas", id: "citas", text: "Citas", icon: "bi-calendar-event", active: false,
+          { href: "/citas", id: "citas", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Quotes"
+              case "/es/": return "Citas"
+              case "/pt/": return "Eventos"
+              case "/fr/": return "Événements"
+              default: return "Citas"
+            }
+          })(), icon: "bi-calendar-event", active: false,
               path1:"M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708",
               path2:""
           },
-          { href: "/reservas", id: "reservas", text: "Reservas", icon: "bi-calendar-event", active: false,
+          { href: "/reservas", id: "reservas", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Reservations"
+              case "/es/": return "Reservas"
+              case "/pt/": return "Reservas"
+              case "/fr/": return "Réservations"
+              default: return "Reservas"
+            }
+          })(), icon: "bi-calendar-event", active: false,
               path1:"M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708",
               path2:""
           },
-          { href: "/nosotros",id: "nosotros", text: "Nosotros", icon: "bi-buildings-fill", active: false,
+          { href: "/nosotros",id: "nosotros", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Us"
+              case "/es/": return "Nosotros"
+              case "/pt/": return "Nós"
+              case "/fr/": return "Nous"
+              default: return "Nosotros"
+            }
+          })(), icon: "bi-buildings-fill", active: false,
             path1:"M15 .5a.5.5 0 0 0-.724-.447l-8 4A.5.5 0 0 0 6 4.5v3.14L.342 9.526A.5.5 0 0 0 0 10v5.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V14h1v1.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5zM2 11h1v1H2zm2 0h1v1H4zm-1 2v1H2v-1zm1 0h1v1H4zm9-10v1h-1V3zM8 5h1v1H8zm1 2v1H8V7zM8 9h1v1H8zm2 0h1v1h-1zm-1 2v1H8v-1zm1 0h1v1h-1zm3-2v1h-1V9zm-1 2h1v1h-1zm-2-4h1v1h-1zm3 0v1h-1V7zm-2-2v1h-1V5zm1 0h1v1h-1z",
             path2:""
           },
-          { href: "/ubicacion", id:"ubicacion", text: "Ubicación", icon: "bi-geo-fill", active: true,
+          { href: "/ubicacion", id:"ubicacion", text: (() => {
+            switch (idioma) {
+              case "/en/": return "Us"
+              case "/es/": return "Ubicación"
+              case "/pt/": return "Nós"
+              case "/fr/": return "Nous"
+              default: return "Ubicación"
+            }
+          })(), icon: "bi-geo-fill", active: true,
             path1:"M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411",
             path2:""
           },
@@ -158,11 +239,7 @@ export const generalConfig = {
       { href: "/contacto", id:"contacto", text: "Contacto", icon: "bi-chat-fill", active: true,
         path1:"M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15",
         path2:""
-      },
-      { href: "/idioma", id:"idioma", text: "Idioma", icon: "bi-flag-fill", active: true,
-        path1:"M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001",
-        path2:""
-      },
+      }
       ],
 
   },  
